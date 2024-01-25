0f5119dbfe7f9d4ebd8ec4e6dd45646b1262791a
Wesley-Dominica <wesley152@icloud.com>
2024-01-25 18:50:40 -0500

    Created Godot stuff

--- PONG 2/player_controller.cs
+++ PONG 2/player_controller.cs
@@ -11,6 +11,5 @@ public partial class player_controller : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		
 	}
 }

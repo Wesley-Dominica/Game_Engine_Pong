0f5f155acf76f84e26c511f126050281a89dc02c
Wesley-Dominica <wesley152@icloud.com>
2024-01-25 23:33:14 -0500

    Godot movement not working

--- PONG 2/Character Scenes/player_bar.cs
+++ PONG 2/Character Scenes/player_bar.cs
@@ -3,4 +3,19 @@ using System;
 
 public partial class player_bar : CharacterBody2D
 {
+    [Export]
+    public int Speed { get; set; } = 400;
+
+    public void GetInput()
+    {
+            Vector2 inputDirection = inputDirection.GetVector("left", "right", "up", "down");
+            Velocity = inputDirection * Speed;
+    }
+
+
+    public override void _PhysicsProcess(double delta)
+    {
+        GetInput();
+        MoveAndSlide();
+    }
 }
